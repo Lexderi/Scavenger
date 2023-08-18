95096da833d0555a41c8a01b2341638da50e0632
Bluestimes <nikom-d@t-online.de>
2023-08-19 01:39:39 +0200

    Added slot generation

--- Assets/Scripts/Inventory/InventoryController.cs
+++ Assets/Scripts/Inventory/InventoryController.cs
@@ -1,15 +1,19 @@
 using System;
 using MyBox;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour
 {
     [Separator("Variables")]
     [SerializeField] [InitializationField] private bool isOpen;
     public bool IsOpen => isOpen;
+    [SerializeField] [InitializationField] [PositiveValueOnly] private Vector2Int gridDimensions;
 
     [Separator("References")]
     [SerializeField] private Canvas canvas;
+    [SerializeField] private GridLayoutGroup gridLayoutGroup;
+    [SerializeField] private GameObject slotPrefab;
 
     private void Start()
     {
@@ -26,5 +30,29 @@ public class InventoryController : MonoBehaviour
     {
         isOpen = !IsOpen;
         canvas.gameObject.SetActive(isOpen);
+    }
+
+    [ButtonMethod]
+    private void GenerateSlots()
+    {
+        // clear slots (not that easy because of edit mode and array order and stuff)
+        GameObject[] tempArray = new GameObject[gridLayoutGroup.transform.childCount];
+        for (int i = 0; i < tempArray.Length; i++)
+        {
+            tempArray[i] = gridLayoutGroup.transform.GetChild(i).gameObject;
+        }
+        foreach (GameObject child in tempArray)
+        {
+            DestroyImmediate(child);
+        }
+
+        // set column constraint
+        gridLayoutGroup.constraintCount = gridDimensions.x;
+
+        // fill in slots
+        for (int i = 0; i < gridDimensions.x * gridDimensions.y; i++)
+        {
+            Instantiate(slotPrefab, gridLayoutGroup.transform);
+        }
     }
 }
